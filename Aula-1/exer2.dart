dac68f372595740a0ce1544445b85234ddd47522
Luiz Fernando <luizfernandosc@gmail.com>
2023-07-27 18:56:06 -0300

    execicios aula 1

--- Aula-1/exer2.dart
+++ Aula-1/exer2.dart
@@ -1,4 +1,19 @@
 //TIPOS PRIMITIVOS
 void main() {
-  print("object");
+  String redacao =
+      ('as mais melhores ferias da minha vida, ferias marvilhosas');
+  print(redacao);
+  print(redacao.contains('ferias'));
+
+  final String requisitos = ('a redação deve possir de 10 à 100 caracteres ');
+
+  print('a sua redação possui ${redacao.length} caracteres ');
+  int caracteres = redacao.length;
+  if (caracteres >= 10 && caracteres <= 100) {
+    print('redação aceita');
+  } else {
+    print('redação invalida ');
+  }
+
+  print(redacao.replaceAll('mais melhores', 'melhores'));
 }
